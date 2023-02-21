8aed1810fb64d83c0186a3f7882d7eaf190c3f1b
emdad <emdad@ergo-ventures.com>
2023-02-22 03:38:30 +0600

    added logic for showing messages

--- app/src/main/java/com/example/sendfund/view/SendFundFragment.kt
+++ app/src/main/java/com/example/sendfund/view/SendFundFragment.kt
@@ -2,16 +2,20 @@ package com.example.sendfund.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.navArgs
 import com.example.sendfund.R
 import com.example.sendfund.databinding.FragmentSendFundBinding
 import com.example.sendfund.model.LoginResponseModel
 import com.example.sendfund.viewmodel.SendFundViewModel
+import java.lang.Double
 
 class SendFundFragment : Fragment() {
 
@@ -38,10 +42,31 @@ class SendFundFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         userField = navigationArgs.user
         binding.tvName.text = userField.dataFromResponse?.userInfo?.UserName ?: ""
-        binding.tvBalance.text = userField.dataFromResponse?.accountInfo?.balance.toString()?:""
+        binding.etBalance.setText("0")
+        binding.tvbalanceBottom.text =
+            "Balance" + " ${userField.dataFromResponse?.accountInfo?.currency}" + " ${userField.dataFromResponse?.accountInfo?.balance}"
+        binding.etBalance.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var valueiInserted = p0.toString().toInt()
+                var balance = userField.dataFromResponse?.accountInfo?.balance?.toInt() ?: 0
+                if (valueiInserted > balance
+                ) {
+                    binding.rlError.visibility = View.GONE
+                } else {
+                    binding.rlError.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
